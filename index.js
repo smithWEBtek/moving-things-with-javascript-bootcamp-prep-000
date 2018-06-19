ce71864381536c897e2ec8fab68a99e74055829a
Jonathan Hong <jonhong777@gmail.com>
2018-06-19 16:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+function moveDodgerRight(){
+  var removePX = dodger.style.left.replace('px','')
+  var numbers = parseInt(removePX, 10)
+
+  if(numbers > 0){
+    dodger.style.right = `${numbers - 1}px`
+  }
+}
+
